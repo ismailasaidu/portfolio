6b8c4d30bcf3eb6c30349a36adddd9caa3db6c50
Ismaila Saidu <ismailasaidu997@gmail.com>
2023-07-11 21:16:12 +0100

    landing change

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -23,13 +23,13 @@ const Landing = () => {
             <p className='sm:pt-[20px]  '>My name is Abdull - Welcome to Teddie’s UX. Below are some tips to get you started on this website</p>
         </div>
         <div className='flex  h-contain sm:h-[300px]  mt-[70px] sm:mt-[70px] '>
-            <div className='border-l px-[20px] border-r w-[370px]  py-[40px]'>
+            <div className='border-l px-[20px] border-r w-[370px]  py-[40px]  sm:flex sm:items-center sm:justify-center'>
                 <p className='sm:hidden'>Use keyboard shortcuts 1 - 7 to navigate <br></br>between pages. Try press 2, 3, 4, then 1 to <br></br>come back here.<br></br><br></br><br></br><br></br>I enjoy meeting random people and help<br></br> where I can. My open calendar is here</p>
-                <p className='hidden sm:block sm:text-[16px]'> I enjoy meeting random people and help<br></br> where I can. My open calendar is here</p>
+                <p className='hidden sm:block sm:text-[16px] sm:text-center'> I enjoy meeting random people and help<br></br> where I can. My open calendar is here</p>
             </div>
-            <div className='border-r px-[20px] py-[40px] w-[370px]'>
+            <div className='border-r px-[20px] py-[40px] w-[370px] sm:flex sm:items-center sm:justify-center'>
                 <p className='sm:hidden'>Don’t know me yet? My Name’s Abdull, and<br></br> I’m a UX Designer based in Kaduna, Nigeria.<br></br><br></br><br></br><br></br><br></br>Don’t know me yet? My Name’s Abdull, and<br></br> I’m a UX Designer based in Kaduna, Nigeria.</p>
-                <p className='hidden sm:block sm:text-[16px]'>Don’t know me yet? My Name’s Abdull, and<br></br> I’m a UX Designer based in Kaduna, Nigeria.</p>
+                <p className='hidden sm:block sm:text-[16px] sm:text-center '>Don’t know me yet? My Name’s Abdull, and<br></br> I’m a UX Designer based in Kaduna, Nigeria.</p>
             </div>
           
         </div>
